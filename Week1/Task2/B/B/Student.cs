ad230127f668c3fccf9da20ec48dc119a89b74e0
TurarUalikhan <0928ttt@gmail.com>
2019-02-05 16:46:23 +0600

    adsa

---
+++ Week1/Task2/B/B/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B
+{
+    class Student
+    {
+        string name;
+        string gpa;
+        string year;
+        string id;
+        public Student(string n, string g, string i, string y)
+        {
+            name = n;
+            gpa = g;
+            id = i;
+            year = y;
+        }
+
+    }
+}
---
+++ Week2/Task1/A/A/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace A
+{
+    class Program
+    {
+        static bool polindrome(string k)
+        {
+            int n = k.Length;
+            for(int i = 0; i < n;i++)
+            {
+                if(k[i] != k[n - i - 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"C:\Users\Turar\Desktop\text.txt");
+            string k = sr.ReadToEnd();
+            if (polindrome(k))
+            {
+                Console.WriteLine("YES");
+            }else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
